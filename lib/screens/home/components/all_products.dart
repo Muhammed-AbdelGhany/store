ec6abd0e782d69486742e6ff692a36b7467a1f34
unknown <muhammedabdelghany5297@gmail.com>
2021-10-05 14:47:34 +0200

    initial commit

---
+++ lib/screens/home/components/all_products.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/product_card.dart';
+import 'package:shop_app/models/Product.dart';
+
+import '../../../size_config.dart';
+import 'section_title.dart';
+
+class AllProducts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SectionTitle(title: "All Products", press: () {}),
+        ),
+        SizedBox(height: getProportionateScreenWidth(20)),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...List.generate(
+                demoProducts.length,
+                (index) {
+                  return ProductCard(product: demoProducts[index]);
+
+                  // here by default width and height is 0
+                },
+              ),
+              SizedBox(width: getProportionateScreenWidth(20)),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
